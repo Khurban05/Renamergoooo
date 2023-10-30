a824ae96a79fa8f47d91a04b33c21137d87f2155
Khurbanchik <121341095+Khurbanchik@users.noreply.github.com>
2023-10-31 00:48:40 +0500

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -200,7 +200,7 @@ async def vid(bot,update):
      		    ph_path = None
      		    print(e)
      
-     value = 5300572800
+     value = 2900000000
      if value < file.file_size:
          await ms.edit("```Sizga yuborish boshlanmoqda...📤```")
          try:
